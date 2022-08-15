0ae4eee0313b4f983d301d223d741a1299a6912c
siendomiguel <siendomigueldev@gmail.com>
2022-08-15 18:24:17 -0500

    integracion de XMLHTTPRequest (Ya no se utiliza)

---
+++ src/callback/challenge.js
@@ -0,0 +1,34 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi, callback){ //urlApi: no confundir y colocar API
+	let xhttp = new XMLHttpRequest(); //referencia a new XMLHttpRequest
+
+	xhttp.open('GET', urlApi, true); //petición "obtener" con true para habilitarlo
+	xhttp.onreadystatechange = function(event) { //escucha diferentes estados de la solicitud y conocer cuando está disponible la información
+	if(xhttp.readyState === 4) {
+    console.log('Estado de respuesta: ' + xhttp.readyState ); //si el estado ha sido completada la llamada
+		if(xhttp.status === 200 ){ //el servido responde de forma correcta
+			callback(null, JSON.parse(xhttp.responseText)); //dentro de xhttp.responseTex recibimos lo que entrega el servidor en texto y se hace la transformación en JSON
+		}
+	} else {
+		const error = new Error('Error' + urlApi);
+		return callback(error,null); //es null porque no se está regresando ningún dato
+	}
+	}
+	xhttp.send();
+}
+fetchData(`${API}/products`, function(error1, data1){
+  if(error1) return console.error(error1);
+  fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+    if (error2) return console.error (error2);
+    fetchData(`${API}/categories/${data2.category.id}`, function (error3, data3){
+      if (error3) return console.error(error3);
+      console.log(data1[0]);
+      console.log(data2.title);
+      console.log(data3.name);
+    });
+  });
+});
+
+//Callback hell un completo desastre, no hacer nunca
